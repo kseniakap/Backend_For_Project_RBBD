a2340c8b3e706c268a4e9da7eeedab5a636de25f
ksenia_kap_1 <kseniakap096@gmail.com>
2023-12-10 19:19:34 +0300

    Stage-2

--- models/Post.js
+++ models/Post.js
@@ -4,16 +4,16 @@ const PostSchema = new mongoose.Schema(
   {
     username: { type: String },
 
-    title: { type: String, required: true },
-    artist: { type: String, required: true },
-    text: { type: String, required: true },
-    imgUrl: { type: String, default: "" },
+    title: { type: String, required: true },//название песни
+    artist: { type: String, required: true },//исполнитель
+    text: { type: String, required: true },//текст песни
+    imgUrl: { type: String, default: "" },//картинка
 
-    views: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },//количество просмотров
 
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },//автор поста
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-    checkAdmin: { type: Boolean, default: false },
+    checkAdmin: { type: Boolean, default: false },//Проверка админом
   },
   { timestamps: true }
 );
